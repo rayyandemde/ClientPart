f95342bc3f3757d1de3b70645608cdc94d8ee7e0
Rayyan De Muhammad <rayyandemde@gmail.com>
2021-02-08 14:50:22 +0100

    Add ForgotPasswordActivity to manifest

---
+++ app/src/main/java/com/android3/siegertpclient/ui/forgotpassword/ForgotPasswordActivity.kt
@@ -0,0 +1,22 @@
+package com.android3.siegertpclient.ui.forgotpassword
+
+import com.android3.siegertpclient.base.BaseActivity
+
+class ForgotPasswordActivity : BaseActivity() {
+    override fun showProgress() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideProgress() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(errorMessage: String) {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(errorId: Int) {
+        TODO("Not yet implemented")
+    }
+
+}
