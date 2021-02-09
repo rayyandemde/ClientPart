94b00bdd65ba8c53731abf3ec0194f1564841c94
Lukas09 <lukas_ruf@gmx.net>
2021-02-09 01:59:00 +0100

    Added Invitation

---
+++ app/src/main/java/com/android3/siegertpclient/data/invitation/invitationSource/IInvitationDataSource.kt
@@ -0,0 +1,4 @@
+package com.android3.siegertpclient.data.invitation.invitationSource
+
+interface IInvitationDataSource {
+}
---
+++ app/src/main/java/com/android3/siegertpclient/data/invitation/invitationSource/invitationRemote/invitationRemoteDataSource.kt
@@ -0,0 +1,4 @@
+package com.android3.siegertpclient.data.invitation.invitationSource.invitationRemote
+
+class invitationRemoteDataSource {
+}
