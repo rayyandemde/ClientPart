21cb209bf59b3b8de6415d111180f9cad7550c7f
Lukas09 <lukas_ruf@gmx.net>
2021-02-09 23:16:06 +0100

    Worked on InvitationRemote

--- app/src/main/java/com/android3/siegertpclient/data/invitation/invitationSource/invitationRemote/invitationRemoteDataSource.kt
+++ app/src/main/java/com/android3/siegertpclient/data/invitation/invitationSource/invitationRemote/invitationRemoteDataSource.kt
@@ -1,4 +1,12 @@
 package com.android3.siegertpclient.data.invitation.invitationSource.invitationRemote
 
 class invitationRemoteDataSource {
+/*
+    fun createInvitation(invitation : Invitation) {
+
+    }*/
+
+    fun acceptInvitation(userId : String, invitationId : String) {
+
+    }
 }
