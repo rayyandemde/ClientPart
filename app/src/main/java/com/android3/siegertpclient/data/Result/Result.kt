f3602d7c06b90083889c65ca2aa53b88dad1219e
QIQI <18612980825@163.com>
2021-02-17 22:55:12 +0100

    add result

--- app/src/main/java/com/android3/siegertpclient/data/Result/Result.kt
+++ app/src/main/java/com/android3/siegertpclient/data/Result/Result.kt
@@ -1,22 +1,17 @@
-package com.android3.siegertpclient.data.game
+package com.android3.siegertpclient.data.Result
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity
-data class Game(
+data class Result (
     @PrimaryKey
     var gameId: String,
-    @ColumnInfo(name = "time") var time: String,
-    @ColumnInfo(name = "result") var result: String,
-    @ColumnInfo(name = "typeOfGame") var typeOfGame: String,
     @ColumnInfo(name = "firstParticipantId") var firstParticipantId: String,
     @ColumnInfo(name = "secondParticipantId") var secondParticipantId: String,
     @ColumnInfo(name = "firstParticipantResult") var firstParticipantResult: String,
     @ColumnInfo(name = "secondParticipantResult") var secondParticipantResult: String,
     @ColumnInfo(name = "firstParticipantScore") var firstParticipantScore: String,
     @ColumnInfo(name = "secondParticipantScore") var secondParticipantScore: String,
-
 )
-
