8813c1c5e31028975109ed33e4e424d0094d0b2b
Lukas09 <lukas_ruf@gmx.net>
2021-02-19 00:09:04 +0100

    Changed Game and Tournament Remote to Call

--- app/src/main/java/com/android3/siegertpclient/data/game/gamesource/gameRemote/GameRemoteDataSource.kt
+++ app/src/main/java/com/android3/siegertpclient/data/game/gamesource/gameRemote/GameRemoteDataSource.kt
@@ -5,18 +5,28 @@ import com.android3.siegertpclient.data.game.Game
 class GameRemoteDataSource (private val gameService: GameService) {
 
     fun getGameById(tournamentName : String, gameId : String, ownUserId : String) : Game {
-        val response = gameService.getGameById(tournamentName, gameId, ownUserId)
+        val userCall = gameService.getGameById(tournamentName, gameId, ownUserId)
+        val response = userCall.execute()
+        if (!response.isSuccessful) {
+
+            //TOdo implement error code
+        }
         return response.body()
     }
 
     fun updateGameById(tournamentName : String, gameId : String, firstWins : Boolean,
                        secondWins : Boolean, draws : Boolean, ownUserId : String) : Game {
-        val response = gameService.updateGameById(tournamentName, gameId, firstWins, secondWins, draws, ownUserId)
+        val userCall = gameService.updateGameById(tournamentName, gameId, firstWins, secondWins, draws, ownUserId)
+        val response = userCall.execute()
+        if (!response.isSuccessful) {
+
+            //TOdo implement error code
+        }
         return response.body()
     }
 
     fun deleteGame(tournamentName : String, gameId : String, ownUserId : String) {
-        val response = gameService.deleteGame(tournamentName, gameId, ownUserId)
+        val userCall = gameService.deleteGame(tournamentName, gameId, ownUserId)
     }
 
 }
--- app/src/main/java/com/android3/siegertpclient/data/game/gamesource/gameRemote/GameService.kt
+++ app/src/main/java/com/android3/siegertpclient/data/game/gamesource/gameRemote/GameService.kt
@@ -1,6 +1,7 @@
 package com.android3.siegertpclient.data.game.gamesource.gameRemote
 
 import com.android3.siegertpclient.data.game.Game
+import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.*
 
@@ -8,12 +9,12 @@ interface GameService {
 
     @GET("tournaments/{tournamentName}/games/{id}")
     fun getGameById(@Path("tournamentName") tournamentName : String,
-                    @Path("id") gameId : String, @Header("currentUserId") ownUserId : String) : Response<Game>
+                    @Path("id") gameId : String, @Header("currentUserId") ownUserId : String) : Call<Game>
 
     @PUT("tournaments/{tournamentName}/games/{id}")
     fun updateGameById(@Path("tournamentName") tournamentName : String,
                        @Path("id") gameId : String, @Body firstWins : Boolean, secondWins : Boolean, draws : Boolean,
-                       @Header("currentUserId") ownUserId : String) : Response<Game>//TODO maybe change body to result
+                       @Header("currentUserId") ownUserId : String) : Call<Game>//TODO maybe change body to result
 
     @DELETE("tournaments/{tournamentName}/games/{id}")
     fun deleteGame(@Path("tournamentName") tournamentName : String,
