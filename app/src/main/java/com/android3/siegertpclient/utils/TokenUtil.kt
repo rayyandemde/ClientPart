ba7441efdfbb47b1ffe67545f4e5aa8aeb793964
irvansian <irvansian@gmail.com>
2021-02-19 22:14:01 +0100

    Change implementation of TokenUtil

--- app/src/main/java/com/android3/siegertpclient/utils/TokenUtil.kt
+++ app/src/main/java/com/android3/siegertpclient/utils/TokenUtil.kt
@@ -1,16 +1,16 @@
 package com.android3.siegertpclient.utils
 
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GetTokenResult
 
 class TokenUtil {
+
     companion object {
         fun getBearerToken(): String {
             var token = "Bearer "
             val auth = FirebaseAuth.getInstance()
             val user = auth.currentUser
-            user?.getIdToken(true)?.addOnSuccessListener { it ->
-                token += it.token.toString()
-            }
+            token += user?.getIdToken(true)?.result?.token
             return token
         }
     }
