74d56cb71f50d8cc76ddfb46a03e79531c8d1479
QIQI <18612980825@163.com>
2021-02-19 19:23:14 +0100

    s

---
+++ app/src/main/java/com/android3/siegertpclient/ui/team/TeamInvitationFragment 2.kt
@@ -0,0 +1,19 @@
+package com.android3.siegertpclient.ui.team
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.android3.siegertpclient.R
+import com.android3.siegertpclient.data.user.User
+
+class TeamInvitationFragment : Fragment() {
+
+    private val teamPresenter: TeamPresenter = TeamPresenter()
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater!!.inflate(R.layout.fragment_feed, container, false)
+    }
+
+}
