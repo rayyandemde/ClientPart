860df138f7346ea0a783ead20d041ce768ecea6f
Rayyan De Muhammad <rayyandemde@gmail.com>
2021-03-08 02:36:12 +0100

    Remove experimental retrofit folders

--- app/src/main/java/com/android3/siegertpclient/ui/dummyretrofit/util/Contants.kt
+++
@@ -1,7 +0,0 @@
-package com.android3.siegertpclient.ui.dummyretrofit.util
-
-class Contants2 {
-    companion object {
-        const val BASE_URL = "https://sieger-teamthree.herokuapp.com/"
-    }
-}
