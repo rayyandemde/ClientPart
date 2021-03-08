b4c9374ad956deedc5b01b2a71ef80deaa1a0003
Rayyan De Muhammad <rayyandemde@gmail.com>
2021-03-08 02:31:45 +0100

    Tidy settings UI code

--- app/src/main/java/com/android3/siegertpclient/ui/setting/SettingActivity.kt
+++ app/src/main/java/com/android3/siegertpclient/ui/setting/SettingActivity.kt
@@ -6,6 +6,8 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import com.android3.siegertpclient.R
+import com.android3.siegertpclient.databinding.ActivityRegisterBinding
+import com.android3.siegertpclient.databinding.ActivitySettingBinding
 import com.android3.siegertpclient.ui.base.BaseActivity
 import com.android3.siegertpclient.ui.homepage.HomepageActivity
 import com.android3.siegertpclient.ui.login.LoginActivity
@@ -14,32 +16,20 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.firebase.auth.FirebaseAuth
 
 class SettingActivity : BaseActivity() , SettingContract.ISettingView {
-
+    private lateinit var binding: ActivitySettingBinding
     private val settingPresenter: SettingPresenter = SettingPresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
 
-        val user = FirebaseAuth.getInstance().currentUser!!
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val backBtn: FloatingActionButton = findViewById(R.id.backButtonSetting)
-        backBtn.setOnClickListener{
+        binding.fabBack.setOnClickListener{
             settingPresenter.onBackBtnClicked()
         }
 
-        val languageBt : Button = findViewById(R.id.changeLanguage)
-
-        val firstnameEt : EditText = findViewById(R.id.changeFirstName)
-
-        val lastnameEt: EditText = findViewById(R.id.changeLastName)
-
-        val testEt: TextView = findViewById(R.id.tv_test_id)
-        testEt.setText("The user is :: ".plus(user.uid))
-
-        val saveBt : Button = findViewById(R.id.saveSettings)
-        val logoutTv: TextView = findViewById(R.id.logout)
-        logoutTv.setOnClickListener{
+        binding.tvLogout.setOnClickListener{
             FirebaseAuth.getInstance().signOut()
             startActivity(Intent(this@SettingActivity, LoginActivity::class.java))
             finish()
@@ -85,5 +75,4 @@ class SettingActivity : BaseActivity() , SettingContract.ISettingView {
     override fun showNoInternetConnection() {
         TODO("Not yet implemented")
     }
-
 }
