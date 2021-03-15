06a859cbba4645bc8fa6ffaf46a730d05a2fdc99
irvansian <irvansian@gmail.com>
2021-03-15 21:26:32 +0100

    Add Result model

--- app/src/main/java/com/android3/siegertpclient/data/tournament/Result.kt
+++ app/src/main/java/com/android3/siegertpclient/data/tournament/Result.kt
@@ -1,4 +1,7 @@
 package com.android3.siegertpclient.data.tournament
 
-class Result {
-}
+data class Result(
+    val firstParticipantResult: String,
+    val secondParticipantResult: String,
+    val type: String
+)
