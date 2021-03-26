11024d21c4230e2910b22d4f3ca11f2ffb64016c
Lukas09 <lukas_ruf@gmx.net>
2021-03-26 21:34:09 +0100

    Created basic structure for invitation tests

---
+++ app/src/test/java/com/android3/siegertpclient/data/invitation/InvitationRemoteDataSourceTest.kt
@@ -0,0 +1,21 @@
+package com.android3.siegertpclient.data.invitation
+
+import com.android3.siegertpclient.data.invitation.invitationsource.invitationRemote.InvitationRemoteDataSource
+import org.junit.Assert
+import org.junit.Test
+
+class InvitationRemoteDataSourceTest {
+
+    private var inviRemote = InvitationRemoteDataSource()
+
+    @Test//TODO update when function is implemented
+    fun createInviTest(){
+        var test = inviRemote.createInvitation("", "", "","", "")
+        Assert.assertEquals(null, test)
+    }
+
+    @Test//TODO update when function is implemented
+    fun handleInvitationTest() {
+        inviRemote.handleInvitationAcceptation("", "", true, "")
+    }
+}
