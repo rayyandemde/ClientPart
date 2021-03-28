fe75f96c1ee3b511f6b02f351077ac94c86ecccb
QIQI <18612980825@163.com>
2021-03-28 21:34:04 +0200

    add ui tests

--- app/src/androidTest/java/com/android3/siegertpclient/ui/createteam/CreateTeamActivityTest.kt
+++ app/src/androidTest/java/com/android3/siegertpclient/ui/createteam/CreateTeamActivityTest.kt
@@ -4,7 +4,9 @@ package com.android3.siegertpclient.ui.createteam
 
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
@@ -16,62 +18,28 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 import androidx.test.espresso.matcher.ViewMatchers.*
+import com.android3.siegertpclient.ui.ToastMatcher
 import com.android3.siegertpclient.ui.homepage.HomepageActivity
+import com.android3.siegertpclient.ui.login.LoginActivity
 
 @RunWith(AndroidJUnit4ClassRunner::class)
 class CreateTeamActivityTest{
-    /**
-     * To test if the navigationbar can work.
-     */
-    @Test
-    fun test_NavToCreateTournament() {
-        //set up
-        val activityScenario = ActivityScenario.launch(HomepageActivity::class.java)
-
-        //Nav Fragment createTournament
-        onView(withId(R.id.navigation_createtournament)).perform(click())
-
-        //vertify
-        onView(withId(R.id.create_tournament_container)).check(matches(isDisplayed()))
-
-
-    }
-    /**
-     * To test if the navigationbar can work.
-     */
-    @Test
-    fun test_NavToJoinTeam() {
-        //set up
-        val activityScenario = ActivityScenario.launch(HomepageActivity::class.java)
-
-        //Nav fragment to jointeam page
-        onView(withId(R.id.navigation_jointeam)).perform(click())
-        onView(withId(R.id.join_team_container)).check(matches(isDisplayed()))
-    }
 
     /**
-     * To test if the navigationbar can work.
+     * To test if click the button can navigate to team page.
      */
     @Test
-    fun test_NavToUserprofile() {
-        //set up
-        val activityScenario = ActivityScenario.launch(HomepageActivity::class.java)
-        //Nav fragment to userprofile page
-        onView(withId(R.id.navigation_userprofile)).perform(click())
-        onView(withId(R.id.userprofile_container)).check(matches(isDisplayed()))
+    fun test_NavToTeamPage() {
+        //Given
+        val activityScenario = ActivityScenario.launch(CreateTeamActivity::class.java)
+        val expected_email = "115@111.com"
+        val password = "111111"
 
-    }
+        //enter some input
+        onView(withId(R.id.et_team_name)).perform(ViewActions.typeText(expected_email))
+        onView(withId(R.id.et_password)).perform(ViewActions.typeText(password))
+        onView(withId(R.id.btn_create)).perform(click())
 
-    /**
-     * To test if the navigationbar can work.
-     */
-    @Test
-    fun test_NavToCreateTeam() {
-        //set up
-        val activityScenario = ActivityScenario.launch(HomepageActivity::class.java)
-        //Nav fragment to createteam page
-        onView(withId(R.id.navigation_createteam)).perform(click())
-        onView(withId(R.id.create_team_container)).check(matches(isDisplayed()))
-
+        onView(withId(R.id.team_container)).check(matches(isDisplayed()))
     }
 }
--- app/src/androidTest/java/com/android3/siegertpclient/ui/tournament/TournamentActivityTest.kt
+++ app/src/androidTest/java/com/android3/siegertpclient/ui/tournament/TournamentActivityTest.kt
@@ -1,5 +1,80 @@
 package com.android3.siegertpclient.ui.tournament
 
-import org.junit.Assert.*
+import com.android3.siegertpclient.data.tournament.Tournament
+import com.android3.siegertpclient.ui.team.TeamActivity
 
-class TournamentActivityTest
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import com.android3.siegertpclient.R
+
+import org.junit.Assert.*
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import androidx.test.espresso.matcher.ViewMatchers.*
+
+
+@RunWith(AndroidJUnit4ClassRunner::class)
+class TournamentActivityTest{
+    /**
+     * To test if the navigationbar can work.
+     */
+    @Test
+    fun test_NavToTournamentDetail() {
+        //set up
+        val activityScenario = ActivityScenario.launch(TournamentActivity::class.java)
+
+        //Nav Fragment createTournament
+        onView(withId(R.id.navigation_tournament_info)).perform(click())
+
+        //vertify
+        onView(withId(R.id.tournament_detail_container)).check(matches(isDisplayed()))
+
+
+    }
+    /**
+     * To test if the navigationbar can work.
+     */
+    @Test
+    fun test_NavToParticipant() {
+        //set up
+        val activityScenario = ActivityScenario.launch(TournamentActivity::class.java)
+
+        //Nav fragment to jointeam page
+        onView(withId(R.id.navigation_participants)).perform(click())
+        onView(withId(R.id.participant_container)).check(matches(isDisplayed()))
+    }
+
+    /**
+     * To test if the navigationbar can work.
+     */
+    @Test
+    fun test_NavToSchedules() {
+        //set up
+        val activityScenario = ActivityScenario.launch(TournamentActivity::class.java)
+        //Nav fragment to userprofile page
+        onView(withId(R.id.navigation_schedule)).perform(click())
+        onView(withId(R.id.schedules_container)).check(matches(isDisplayed()))
+
+    }
+
+    /**
+     * To test if the navigationbar can work.
+     */
+    @Test
+    fun test_NavToMatches() {
+        //set up
+        val activityScenario = ActivityScenario.launch(TournamentActivity::class.java)
+        //Nav fragment to userprofile page
+        onView(withId(R.id.navigation_match_info)).perform(click())
+        onView(withId(R.id.matches_container)).check(matches(isDisplayed()))
+
+    }
+
+}
